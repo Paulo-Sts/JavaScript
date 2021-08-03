5c4f54a6808354bde12b623a7fdf560c1a9da855
Paulo Santos <pauloed_@outlook.com>
2021-08-03 16:24:22 -0300

    Operador ternário

---
+++ Introdução/operadorTernario.js
@@ -0,0 +1,4 @@
+//Esse operador estabelece uma relação entre três partes: parte1 = expressão relacional ? parte2 = resultadoVerdadeiro : parte3 = resultadoFalso
+let mediaProva = 8
+let resultado = mediaProva >= 7 ? "Aprovado":"Reprovado"
+console.log(resultado)
