d1b34a6326e0a2c9244650a1758db483be2186c5
Paulo Santos <pauloed_@outlook.com>
2021-08-03 16:21:11 -0300

    biblioteca math

---
+++ Introdução/math.js
@@ -0,0 +1,7 @@
+//O Math é um objeto JavaScript com uma série de funções 
+
+//Exemplo Área da Circunferência
+const raio = 6.8
+const area = Math.PI * Math.pow(raio, 2)
+console.log(area)
+console.log(typeof Math) 
