fdeb8a6a7797747e0d68f0c14599dea58a182fd7
Paulo Santos <pauloed_@outlook.com>
2021-08-03 16:22:57 -0300

    tipos numéricos

---
+++ Introdução/numeros.js
@@ -0,0 +1,23 @@
+//Tipos númericos são definidos como number
+
+const nota1 = 6.0
+const nota2 = 9.3
+console.log(typeof nota1)
+console.log(typeof nota2)
+
+const total = Number(nota1 + nota2) // A função Number fornece um conjunto de métodos para manipulação de números
+console.log(total)
+console.log(Number)
+
+const media = total / 2
+console.log(media)
+console.log(media.toFixed(1)) // Método Number para definir a quantidade de casas decimais
+
+/* JavaScript permite algumas interações 
+* diferentes de outras linguagens
+*/
+let divisaoPorZero = 10 / 0
+console.log(divisaoPorZero)
+
+let operacaoNumeroString = "20" / 2
+console.log(operacaoNumeroString)
