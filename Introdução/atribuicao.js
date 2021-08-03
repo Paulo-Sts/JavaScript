b14ea735c48a7ff2d8c825e4f0c64a440cabc54e
Paulo Santos <pauloed_@outlook.com>
2021-08-03 16:19:27 -0300

    fundamentos atribuicao

---
+++ Introdução/atribuicao.js
@@ -0,0 +1,22 @@
+//Atribuição de valor
+let numero1 = 40
+let numero2 = 60
+
+//Atribuição com operações aritméticas
+numero1 += numero2 //numero1 = numero1 + numero2
+console.log(numero1)
+
+numero1 -= numero2 //numero1 = numero1 - numero2
+console.log(numero1)
+
+numero1 *= 2 //numero1 = numero1 * 2
+console.log(numero1)
+
+numero1 /= 2 //numero1 = numero1 / 2
+console.log(numero1)
+
+numero1 %= numero2 //numero1 = numero1 % 2
+console.log(numero1)
+
+numero1 **= 3 //numero1 * numero1 * numero1
+console.log(numero1)
