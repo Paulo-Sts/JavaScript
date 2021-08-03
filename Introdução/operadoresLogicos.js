5c13b5db291c2370c474df6305c7e2b3d12d394c
Paulo Santos <pauloed_@outlook.com>
2021-08-03 16:25:19 -0300

    Operadores lógicos

---
+++ Introdução/operadoresLogicos.js
@@ -0,0 +1,7 @@
+//Os operadores lógicos estabelecem uma relação
+let teste = true
+
+console.log("Ou:",teste || teste) //Operador OU 
+console.log("E:",teste && teste) //Operador E
+console.log("Negação:",!teste) //Operador de Negação
+console.log("Ou Exclusivo:",!teste || teste) //Artifício para criar um operador de OU Exclusivo
