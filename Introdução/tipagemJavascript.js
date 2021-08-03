6bfa2ac788731064511d37568e1fe4e8ea3cc06f
Paulo Santos <pauloed_@outlook.com>
2021-08-03 16:26:53 -0300

    Tipagem de dados em Js

---
+++ Introdução/tipagemJavascript.js
@@ -0,0 +1,12 @@
+/* JavaScript possui tipagem dinâmica, sendo fracamente
+ tipada, ou seja, uma variável pode é capaz de armazenar
+ dados de diferentes tipos ao longo do tempo
+ */
+
+let variavelUm = 700 //variável como number
+console.log(variavelUm)
+console.log(typeof variavelUm)
+
+variavelUm = "Olá Mundo" //variável como string
+console.log(variavelUm)
+console.log(typeof variavelUm)
