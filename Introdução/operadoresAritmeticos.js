a6f187f51b1576d579e3915fe54676e1d5ccd53d
Paulo Santos <pauloed_@outlook.com>
2021-08-03 16:24:55 -0300

    operadores aritméticos

---
+++ Introdução/operadoresAritmeticos.js
@@ -0,0 +1,11 @@
+//São operadores binários, ou seja, usam dois operandos para realizar a operação
+
+let [a, b, c, d, e] = [30, 5, 60, 34, 78] //Criando 4 variáveis a partir de um destructuring de um array
+
+let soma = a + b
+let subtracao = c - b - a
+let multiplicacao = d * e
+let divisao = c / a
+let modulo = d % 2
+let exponenciacao = b ** 2
+console.log(soma, subtracao, multiplicacao, divisao, modulo, exponenciacao)
