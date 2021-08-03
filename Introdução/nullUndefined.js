c9161ffb9108e72a2754d19584c4a01f1b1f7bde
Paulo Santos <pauloed_@outlook.com>
2021-08-03 16:22:25 -0300

    Null e undefined

---
+++ Introdução/nullUndefined.js
@@ -0,0 +1,7 @@
+//O undefined é definido quando uma variável não está inicializada com um valor
+let valor
+console.log(valor)
+
+//O tipo null é usado para definir que uma variável não aponta para nenhum endereço de memória, ou seja, não referencia nada
+valor = null
+console.log(valor)
