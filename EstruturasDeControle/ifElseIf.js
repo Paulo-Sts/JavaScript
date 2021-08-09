816691c9b9bc1790e156b5686ad54235bda7f388
Paulo Santos <pauloed_@outlook.com>
2021-08-09 11:43:21 -0300

    comando if e else encadeado

---
+++ EstruturasDeControle/ifElseIf.js
@@ -0,0 +1,19 @@
+//Podemos encadear vários ifs, cada um para uma condição
+
+let imprimeResultado = function(resultado){
+    
+    if(resultado >= 7){
+        console.log("Aprovado")
+    } else if(resultado < 7 && resultado >= 5){
+        console.log("Recuperação")
+    } else if(resultado < 5 && resultado >=3){
+        console.log("Prova Final")
+    }  else {
+        console.log("Reprovado")
+    }
+}
+
+imprimeResultado(10)
+imprimeResultado(6)
+imprimeResultado(4)
+imprimeResultado(2)
