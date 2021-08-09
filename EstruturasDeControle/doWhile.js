b9456b3ab9974acf17d44254febfb13020113458
Paulo Santos <pauloed_@outlook.com>
2021-08-09 11:44:02 -0300

    comando do e while

---
+++ EstruturasDeControle/doWhile.js
@@ -0,0 +1,10 @@
+//O do while Executa o bloco ao menos uma vez
+
+let valor = 1
+
+do{
+    console.log("EXECUTA O BLOCO SE VALOR MENOR QUE 10")
+    valor += 1
+} while(valor < 10)
+
+console.log("VALOR MAIOR OU IGUAL A 10, NÃO EXECUTA!")
