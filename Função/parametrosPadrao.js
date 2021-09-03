14bf4946f4a69ab1bc1bbe6f5aeb2692329c69da
Paulo Santos <pauloed_@outlook.com>
2021-09-03 13:03:03 -0300

    Parâmetros padrão

---
+++ Função/parametrosPadrao.js
@@ -0,0 +1,25 @@
+//Existem algumas formas de definir parâmetros padrão, na versão Ecma2015 foi criada a versão mais comum de se definir parâmetros padrão
+//Forma padrão
+function soma1(a = 1, b = 1, c = 1){
+    return a + b + c
+}
+
+console.log(soma1(), soma1(5), soma1(10, 10, 10))
+
+//Outras formas
+function soma2(a, b, c){ //usando o operador ou para atribuir o valor padrão sempre que o primeiro parâmetro da expressão for falso
+    a = a || 1
+    b = b || 1
+    c = c || 1
+    return a + b + c
+}
+
+console.log(soma1(), soma1(5), soma1(10, 10, 10))
+
+function soma3(){
+    a = a !== undefined ? a: 1
+    b = 1 in arguments ? b:1
+    c = isNaN(c) ? 1: c
+}
+
+console.log(soma1(), soma1(5), soma1(10, 10, 10))
