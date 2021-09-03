48fffccadaaf363b99f2e255b0b206552cae03a3
Paulo Santos <pauloed_@outlook.com>
2021-09-03 13:00:58 -0300

    Funções com Callback

---
+++ Função/funcaoCallBack.js
@@ -0,0 +1,10 @@
+//São funções que são chamadas quando um evento acontece
+
+//Exemplo
+const linguagens = ["Python", "Java", "Javascript"]
+
+function imprime(nome, indice){
+    console.log(`${indice + 1}: ${nome}`)
+}
+
+linguagens.forEach(imprime) //O Callback é o processo de chamar a função imprime sempre que passar pelo evento imprimir valor do array
