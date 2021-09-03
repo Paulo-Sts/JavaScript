056ee1297a5723dcc978efb63d7d9e1e98a23ad6
Paulo Santos <pauloed_@outlook.com>
2021-09-03 12:59:18 -0300

    Função Arrow

---
+++ Função/arrowFunction.js
@@ -0,0 +1,23 @@
+//Funções Arrow são uma forma simplificada de definir uma função, além disso ela definem um this que se comporta sempre da mesma forma
+let soma = (a, b) =>{
+    return a + b
+}
+
+soma = (a, b) => a + b //Forma simplificada de escrever, nesse caso o retorno é implícito
+
+let frase = () => "Hello Wolrd"
+frase = _ => "Olá Mundo!" //Podemos abstrair o parâmetro
+
+console.log(soma(2, 2))
+console.log(frase())
+
+//O this não varia quando definido por uma função arrow
+function Pessoa(){
+    this.idade = 0
+    setInterval(() => {
+        this.idade++
+        console.log(this.idade)
+    }, 1000)
+}
+
+new Pessoa
