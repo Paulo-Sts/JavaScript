fa50c18a8233508b1b0aaaebbe0e2ea06af11f13
Paulo Santos <pauloed_@outlook.com>
2021-09-07 21:00:56 -0300

    Definindo estrutura de objeto com função

---
+++ Função/funcaoConstrutura.js
@@ -0,0 +1,39 @@
+//No Javascript podemos definir a estrutura de objetos(sua classe) através de funções
+
+function Carro(velocidadeMaxima = 180, acelaracao = 5){
+
+    //atributo privado
+    let velocidadeAtual = 0
+
+    //Método público acelerar
+    this.acelerar = function(){
+        if(velocidadeAtual + acelaracao <= velocidadeMaxima){
+            velocidadeAtual += acelaracao
+        } else{
+            velocidadeAtual = velocidadeMaxima
+        }
+    }
+
+    //Método ver velocidade
+    this.getVelocidadeAtual = function(){
+        return velocidadeAtual
+    }
+}
+
+//Criando objeto com parâmetros padrão
+const fox = new Carro
+
+fox.acelerar()
+console.log(fox.getVelocidadeAtual())
+fox.acelerar()
+fox.acelerar()
+fox.acelerar()
+fox.acelerar()
+console.log(fox.getVelocidadeAtual())
+
+//Criando novo objeto com outros parâmetros
+const audiR8 = new Carro(400, 100)
+audiR8.acelerar()
+audiR8.acelerar()
+audiR8.acelerar()
+console.log(audiR8.getVelocidadeAtual())
