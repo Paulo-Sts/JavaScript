b7d8c3e63e2cc79cbe2d36fd5fbf82e0eaf216ff
Paulo Santos <pauloed_@outlook.com>
2021-09-10 11:14:17 -0300

    funcao factory

---
+++ Função/funcaoFactory.js
@@ -0,0 +1,13 @@
+//Funções Factory são usadas para a partir de uma função criar objetos
+
+function criaCarro(nome, ano, cor){
+    return{
+        nome: nome,
+        ano: ano,
+        cor: cor
+    }
+}
+
+const carro1 = criaCarro("Fox", 2011, "Cinza")
+
+console.log(carro1)
