2d17b028064dc9b9e9083df14ac0001e1cc37c1d
Paulo Santos <pauloed_@outlook.com>
2021-09-23 17:07:29 -0300

    Método get e set

---
+++ Objetos/getterSetter.js
@@ -0,0 +1,18 @@
+//Não existem atributos privados em Javascript, porém a convenção é usar os getters e setters para manipular o objeto
+//É também convenção usar um underline antes do nome do atributo para informar que este não deve ser acessado diretamente
+
+const sequencia = {
+    _valor: 1,
+    get valor(){
+        return this._valor
+    },
+    set valor(valor){
+        if(valor > this._valor){
+            this._valor = valor
+        }
+    }
+}
+
+console.log(sequencia.valor,sequencia.valor)
+sequencia.valor = 2
+console.log(sequencia.valor)
