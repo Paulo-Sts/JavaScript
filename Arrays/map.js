7076770f27b74c776a7c5b4e0a19fead42b12636
Paulo Santos <pauloed_@outlook.com>
2021-10-18 15:26:14 -0300

    Método map

---
+++ Arrays/map.js
@@ -0,0 +1,8 @@
+//Esse método percorre um array e o transforma em um novo array de igual tamanho porém com dados modificados
+const numeros = [1, 2, 3, 4]
+
+let resultado = numeros.map(function(elemento){ //Criando um novo array com os valores dobrados
+    return elemento * 2
+})
+
+console.log(resultado)
