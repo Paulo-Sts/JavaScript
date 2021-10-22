44107223a6f7296ea87c04e66209d12050457f7d
Paulo Santos <pauloed_@outlook.com>
2021-10-22 11:18:47 -0300

    Método concat

---
+++ Arrays/concat.js
@@ -0,0 +1,6 @@
+//O método concat junta diferentes arrays e dados em um novo array
+const filhos = ["José", "Marco"]
+const filhas = ["Maria", "Josefa"]
+
+const todosOsFilhos = filhos.concat(filhos, "Terry")
+console.log(todosOsFilhos, filhos, filhas)
