042d65cdce772ebff2aae8e8e3f31c0c691734c8
Paulo Santos <pauloed_@outlook.com>
2021-11-12 14:38:54 -0300

    Renomeando diretório exercicios e separando conteúdo node

--- Node/modulosA.js
+++
@@ -1,5 +0,0 @@
-//Formas de exportar um módulo do Node dinamicamente
-
-this.contato = "Oi, tudo bem?"  //Ambos usam objetos para definir um novo elemento desse objeto com sua chave e valor
-exports.saudacao = "Seja bem vindo"
-module.exports.despedida = "Até logo!"
--- Node/modulosB.js
+++
@@ -1,7 +0,0 @@
-//Definindo um objeto para ser exportado 
-module.exports = {
-    saudacao: "Bom dia",
-    saudacaoNoite() {
-        return "Boa noite!"
-    } 
-}
