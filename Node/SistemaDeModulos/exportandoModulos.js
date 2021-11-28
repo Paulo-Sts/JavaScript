82f6ca053af2803e9605a816e6048d0abae0f731
Paulo Santos <pauloed_@outlook.com>
2021-11-28 10:23:53 -0300

    Exportando módulos de arquivos com o node

---
+++ Node/SistemaDeModulos/exportandoModulos.js
@@ -0,0 +1,22 @@
+/*
+O sistema de módulos do Node é usando para estruturar o desenvolvimento de código backend seguindo uma arquitetura
+mais limpa e organizada, sendo cada arquivo u módulo com seus recursos que podem ser acessados por outros módulos
+de acordo com sua função
+*/
+
+//Criando um objeto dinâmicamente
+
+this.mensagemInicial = "Oi, seja bem vindo!"
+exports.segundaMensagem = "Até logo!"
+
+//Atribuindo um novo objeto ao module.exports
+module.exports = {
+    moto: {
+        modelo: "Honda",
+        cor: "Preta",
+        ano: 2018
+    },
+    liga(){
+        return console.log("Moto ligada")
+    }
+}
---
+++ Node/SistemaDeModulos/importandoModulos.js
@@ -0,0 +1,11 @@
+//Para imortar módulos usamos o require()
+
+const importandoModulos = require('./exportandoModulos')
+
+//Usando módulos importados
+
+console.log(importandoModulos.segundaMensagem)
+
+console.log(importandoModulos.moto)
+
+console.log(importandoModulos.liga)
