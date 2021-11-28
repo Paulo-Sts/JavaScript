320a83f3dbae10d0c25a3c383031f8484d24a6c6
Paulo Santos <pauloed_@outlook.com>
2021-11-28 10:24:37 -0300

    Entendendo o module.exports

---
+++ Node/SistemaDeModulos/exportar.js
@@ -0,0 +1,13 @@
+//A melhor forma de exportar um módulo é o definindo dentro do module.exports
+
+this.a = 1
+exports.b = 2
+module.exports.c = 3
+
+console.log(module.exports)
+
+//Para redefinir o que se deseja exportar, só é possível usando o module.exports que criará um novo objeto
+
+module.exports = {
+    d: 4
+}
---
+++ Node/SistemaDeModulos/exportarCliente.js
@@ -0,0 +1,4 @@
+const usandoModulo = require('./exportar')
+
+//Ele imprime o novo objeto definido que foi o módulo exportado
+console.log(usandoModulo)
