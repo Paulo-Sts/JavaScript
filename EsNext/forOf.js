fc0c6df85464ab189478d58fa73c03f7fb3dc902
Paulo Santos <pauloed_@outlook.com>
2021-12-27 10:36:42 -0300

    For of

---
+++ EsNext/forOf.js
@@ -0,0 +1,32 @@
+//É um novo tipo de laço que intera a partir de valores
+
+//Interando uma string
+for (let letra of "Paulo"){
+    console.log(letra)
+}
+
+//Interando um array
+const tecs = ['Js', 'Typescript', 'Vue', "Flutter"]
+
+for (let tec of tecs){
+    console.log(tec)
+}
+
+//Interando um Map
+const assuntoMap = new Map([
+    ['Map', {abordado: true}],
+    ['Set', {abordado: true}],
+    ['Promise', {abordado: false}]
+])
+
+for (let assunto of assuntoMap){ //Interando todo o Map
+    console.log(assunto)
+}
+
+for (let chave of assuntoMap.keys()){ //Interando as chaves
+    console.log(chave)
+}
+
+for (let valor of assuntoMap.values()){ //Interando os valores
+    console.log(valor)
+}
