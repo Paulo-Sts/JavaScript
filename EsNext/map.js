86993c048680fe0dba3770b747605c412b163328
Paulo Santos <pauloed_@outlook.com>
2021-12-27 10:36:06 -0300

    Map

---
+++ EsNext/map.js
@@ -0,0 +1,27 @@
+//É uma nova estrutura de dados chave valor em que a chave pode ser formada por diferentes tipos
+const tecnologias = new Map()
+
+tecnologias.set('Javascript', {framework: false})
+
+tecnologias.set('Angular', {framework: true})
+
+console.log(tecnologias.get('Javascript'))
+console.log(tecnologias.get("Javascript").framework)
+
+//Outra forma de criar um Map
+const variadasChaves = new Map([
+    [function () {}, 'Função'],
+    [{}, 'Objeto'],
+    [123, 'Número']
+])
+
+variadasChaves.forEach((vl, ch) => {
+    console.log(ch, vl)
+})
+
+//Métodos do Map
+console.log(variadasChaves.has(123))
+
+console.log(variadasChaves.delete(123))
+
+console.log(variadasChaves.size)
