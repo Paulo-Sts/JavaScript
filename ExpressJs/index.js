2df47b7698980076c2fe8c7a81adbb17345cf745
Paulo Santos <pauloed_@outlook.com>
2021-12-29 12:51:17 -0300

    Entendendo os tipos de resposta e como usar middlewares

--- ExpressJs/index.js
+++ ExpressJs/index.js
@@ -1,7 +1,48 @@
 const express = require('express')
+const funcaoTeste = require('./exemploMiddleware')
 
 //instanciando o express
 const app = express() //Por meio da aplicação instanciada é definido a construção da Api
+
+app.use('/teste',(req, res, next) => { //O next é a implementação do padrão middleware que integra a chamada de métodos de acordo com a sua responsabilidade
+    console.log('Teste1')
+    next()
+})
+
+//Usando um middleware
+app.use(funcaoTeste)
+
+app.get('/teste',(req, res, next) => {
+    //Tipos de Resposta
+   //res.send("Bem Vindo!") //Html
+
+    //res.json({atributo: "valor"}) //Json
+
+    /*res.json([ //Array de objetos Json
+        {atributo1: "valor1"},
+        {atributo2: "valor2"},
+        {atributo3: "valor3"}
+    ])*/
+
+    res.json({ //Dados sobre a requisição
+        data: [
+            {atributo1: "valor1"},
+            {atributo2: "valor2"},
+            {atributo3: "valor3"}
+        ],
+        count: "valor",
+        skip: "valor",
+        limit: "valor",
+        status: "valor"
+    })
+    next()
+})
+
+app.use('/teste',(req, res) => {
+    console.log('Teste2')
+   
+})
+
 
 //Define a porta da aplicação
 app.listen(3000, () => {
