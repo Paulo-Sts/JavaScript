be6af29d70e8f577be476c048a4db2cc4e932ab4
Paulo Santos <pauloed_@outlook.com>
2022-02-14 14:39:17 -0300

    Testando como acessar elementos de um array de objetos

---
+++ ExerciciosJs/acessaElementoArray.js
@@ -0,0 +1,25 @@
+const teste = [
+    {
+      "id": 1,
+      "title": "teste 1",
+      "idCourse": 1,
+      "createdAt": "2021-11-05T10:41:22.000Z",
+      "updatedAt": "2021-11-05T10:41:22.000Z"
+    },
+    {
+      "id": 2,
+      "title": "teste 2",
+      "idCourse": 1,
+      "createdAt": "2021-11-05T10:41:22.000Z",
+      "updatedAt": "2021-11-05T10:41:22.000Z"
+    },
+    {
+      "id": 3,
+      "title": "teste 3",
+      "idCourse": 1,
+      "createdAt": null,
+      "updatedAt": null
+    }]
+
+const mostra = teste[teste.length - 1]
+console.log(mostra.title)
