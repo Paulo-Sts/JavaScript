6998ab0994e63c63121bdaa73b3771eac9d99087
Paulo Santos <paulosantos.fromtech@gmail.com>
2023-12-05 15:58:14 -0300

    review

---
+++ exercicios-js/review.js
@@ -0,0 +1,12 @@
+let nome
+nome = "Paulo"
+nome = "Edu"
+console.log(nome)
+
+const gravidade = 10
+
+console.log(gravidade)
+
+let idade = 17
+let resultado = idade >= 18 ? "de maior" : "de menor"
+console.log(resultado)
